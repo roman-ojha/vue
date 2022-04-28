831b65786cacd6b549ec35de0fa1a39783b23c1f
Roman Ojha <razzroman98@gmail.com>
2022-04-28 08:56:15 +0545

    Event Handling part 2

---
+++ Class/src/Topic_Wise/18_Event_Handling_02/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- Using Short hand Syntax =================== -->
+  <h2>{{ name }}</h2>
+  <div>
+    <button @click="changeName">Change name on Click</button>
+  </div>
+  <h2>{{ count }}</h2>
+  <div>
+    <button @click="increment(5, $event)">Increment</button>
+    <!-- In user passed argument Vue doesn't pass event in default we have to pass '$event' to pass the event -->
+    <button @click="decrement(5)">Decrement</button>
+  </div>
+
+  <!-- Call Multiple Function on Event -->
+  <button @click="changeName($event), increment(10, $event)">
+    <!-- In this case in 'changeName' Vue will not pass event as argument by default -->
+    Change name and Increment
+  </button>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      name: "Roman",
+      count: 0,
+    };
+  },
+  methods: {
+    changeName(e) {
+      // We can also be able to access event of event
+      console.log(e);
+      this.name = "batman";
+    },
+    increment(payload, event) {
+      console.log(event);
+      this.count += payload;
+    },
+    decrement(payload) {
+      this.count -= payload;
+    },
+  },
+};
+</script>
+
+<style></style>
