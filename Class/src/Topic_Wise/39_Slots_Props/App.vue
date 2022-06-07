4c8280055622b80b92ac25bf843aa6eec5025a2f
Roman Ojha <razzroman98@gmail.com>
2022-06-07 13:27:54 +0545

    docs(tutorial): Slots Props

---
+++ Class/src/Topic_Wise/39_Slots_Props/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <NameList>
+    <!-- we know that the data property of name list contain in the 'NameList' component not in the 'App' component to get the data that is present in child component we need to send data from child to parent for that we use slot props, it is similar to props but we are applying those data in slots within a component -->
+
+    <!-- now after passing data as slot props we can access here -->
+    <template v-slot:default="slotProps">
+      <!-- we need to indicate to vue that this template belongs to the slot in our 'NameList' component for that we need to specify the slot name: default -->
+      <!-- and on the right hand side of this directive will give us access to the slot props send from the child component -->
+      <!-- 'slotProps' content all the props that is send from the child -->
+      {{ slotProps.lastName }}
+    </template>
+  </NameList>
+
+  <NameList>
+    <template v-slot:default="slotProps">
+      {{ slotProps.lastName }} {{ slotProps.firstName }}
+    </template>
+  </NameList>
+
+  <NameList>
+    <template v-slot:default="slotProps">
+      {{ slotProps.firstName }} {{ slotProps.lastName }}
+    </template>
+  </NameList>
+</template>
+
+<script>
+import NameList from "./components/NameList.vue";
+export default {
+  name: "App",
+  components: {
+    NameList,
+  },
+};
+</script>
+
+<style></style>
---
+++ Class/src/Topic_Wise/39_Slots_Props/components/NameList.vue
@@ -0,0 +1,35 @@
+<template>
+  <h3 v-for="name in names" :key="name.firstName">
+    <!-- {{ name.firstName }} {{ name.lastName }} -->
+    <!-- So if sometimes we just want to display fist name and sometimes we want to display full name it means that we want parent component to control how the child component render the content in this kind of case we can user slots -->
+
+    <!-- to send data to parent component we will add to props on slot tag by using v-bind directive -->
+    <slot :firstName="name.firstName" :lastName="name.lastName"></slot>
+  </h3>
+</template>
+
+<script>
+export default {
+  name: "NameList",
+  data() {
+    return {
+      names: [
+        {
+          firstName: "Bruce",
+          lastName: "Wayne",
+        },
+        {
+          firstName: "Clark",
+          lastName: "Kent",
+        },
+        {
+          firstName: "Princess",
+          lastName: "Diana",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
